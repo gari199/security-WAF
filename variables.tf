067085138fae387366cf3aac8ec5be3ce82fd1ca
gari199 <luis-lim@MacArra-20.local>
2024-06-24 19:13:07 +0200

    subnet ids

--- variables.tf
+++ variables.tf
@@ -1,9 +1,3 @@
-variable "tags" {
-  type        = map(string)
-  description = "Tag that should be applied to all resources"
-  default     = {}
-}
-
 variable "project_name" {
   description = "Name of the project"
   type        = string
@@ -21,3 +15,19 @@ variable "security_group_id" {
   type = string
   default ="sg-0c226fddab0a2860c"
 }
+
+variable "subnet_id_1" {
+  description = "SUbnet ID 1"
+  type = string
+}
+
+variable "subnet_id_2" {
+  description = "SUbnet ID 1"
+  type = string
+}
+
+variable "subnet_id_3" {
+  description = "SUbnet ID 1"
+  type = string
+}
+
