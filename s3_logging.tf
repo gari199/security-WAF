021c13c0546458cab588ef027e68a94be8e2c757
gari199 <luiscobovazquez@hotmail.com>
2025-11-19 17:25:19 +0100

    reduce bucket complexity

--- s3_logging.tf
+++ s3_logging.tf
@@ -32,39 +32,3 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
     }
   }
 }
-
-#Bucket lifecycle configuration
-resource "aws_s3_bucket_lifecycle_configuration" "bucket-config" {
-  bucket = aws_s3_bucket.acme-demo-waf-security.id
-
-  rule {
-    id = "log"
-    filter {
-      and {
-        prefix = "log/"
-
-        tags = {
-          rule      = "log"
-          autoclean = "true"
-        }
-      }
-    }
-
-    status = "Enabled"
-
-    transition {
-      days          = 30
-      storage_class = "ONEZONE_IA"
-    }
-
-    transition {
-      days          = 60
-      storage_class = "GLACIER"
-    }
-
-    expiration {
-      days = 90
-    }
-
-  }
-}
